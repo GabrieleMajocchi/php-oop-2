5f8ba5e81b29df4b3cb821f6660d2348399ee745
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-07-14 15:49:55 +0200

    created index with elements

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+    include_once __DIR__ . '/db/db.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zoo Planet</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="./styles/style.css">
+
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-around">
+            <div class="col-12">
+                <h1>
+                    Zoo Planet
+                </h1>
+            </div>
+            <div class="col-12">
+                <h2>
+                    Products:
+                </h2>
+            </div>
+            
+        </div>
+    </div>
+</body>
+</html>
